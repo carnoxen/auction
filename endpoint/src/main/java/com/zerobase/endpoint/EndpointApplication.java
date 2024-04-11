d3a84d5a3a341f2740c25f83eb489b1f0cdb0919
carnoxen <carnoxen@gmail.com>
2024-04-11 22:40:06 +0900

    add scanpackages

--- endpoint/src/main/java/com/zerobase/endpoint/EndpointApplication.java
+++ endpoint/src/main/java/com/zerobase/endpoint/EndpointApplication.java
@@ -3,7 +3,7 @@ package com.zerobase.endpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zerobase")
 public class EndpointApplication {
     public static void main(String[] args) {
         SpringApplication.run(EndpointApplication.class, args);
