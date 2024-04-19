8ce1c4d6d85149554637d7bfbe04ec1f92523ed0
carnoxen <carnoxen@gmail.com>
2024-04-20 01:49:19 +0900

    * Modify GeneratedValue to Tsid * Remove springapplication from library module and fix build.gradle * Add @EnableJpaRepositories, @EntityScan * Add async logic to BidService

--- endpoint/src/main/java/com/zerobase/endpoint/EndpointApplication.java
+++ endpoint/src/main/java/com/zerobase/endpoint/EndpointApplication.java
@@ -1,11 +1,32 @@
 package com.zerobase.endpoint;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.config.TopicBuilder;
 
-@SpringBootApplication(scanBasePackages = "com.zerobase")
+import com.zerobase.messaging.KafkaProperties;
+
+@SpringBootApplication(scanBasePackages = {"com.zerobase"})
+@EnableJpaRepositories("com.zerobase.domain.repository")
+@EntityScan("com.zerobase.domain.entity")
 public class EndpointApplication {
+    @Autowired
+    private KafkaProperties kafkaProperties;
+    
     public static void main(String[] args) {
         SpringApplication.run(EndpointApplication.class, args);
+    }
+
+    @Bean
+    public NewTopic bidTopic() {
+        return TopicBuilder.name(kafkaProperties.getTopic())
+            .partitions(kafkaProperties.getPartition())
+            .replicas(kafkaProperties.getReplica())
+            .build();
     }
 }
--- endpoint/src/main/resources/schema.sql
+++ endpoint/src/main/resources/schema.sql
@@ -1,12 +1,10 @@
-drop table if exists user;
-create table if not exists user(
-    id bigint auto_increment primary key,
+create or replace table user(
+    id bigint primary key,
     username varchar(256) not null unique
 );
 
-drop table if exists item;
-create table if not exists item(
-    id bigint auto_increment primary key,
+create or replace table item(
+    id bigint primary key,
     name varchar(256) not null,
     start bigint not null,
     timeout int not null,
@@ -14,9 +12,9 @@ create table if not exists item(
     bidder_id bigint references user(id)
 );
 
-drop table if exists bid;
-create table if not exists bid(
-    id bigint auto_increment primary key,
+-- drop table if exists bid;
+create or replace table bid(
+    id bigint primary key,
     user_id bigint not null references user(id),
     item_id bigint not null references item(id),
     value bigint not null,
